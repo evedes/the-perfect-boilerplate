555c9fa49d20fcd60b1fb7f0929abdab6d7ef0b5
Eduardo Vedes <eduardo.vedes@gmail.com>
2025-10-04 22:49:03 +0100

    feat: initialize frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-2">
+      <span className="text-2xl">The Perfect Boilerplate</span>
+      <span>&copy; 2025 &bull; Eduardo &quot;Edo&quot; Vedes</span>
+    </main>
+  );
+}
